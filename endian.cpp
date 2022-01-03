037afeace5c17ef786b38222eac51d72e761c109
sean417 <373202226@qq.com>
2022-01-03 15:33:02 +0800

    endian

--- endian.cpp
+++ endian.cpp
@@ -2,7 +2,7 @@
 // Created by chenyang on 2022/1/3.
 //
 #include <iostream>
-
+using namespace std;
 int main(){
 
     union {
@@ -12,14 +12,14 @@ int main(){
     un.s=0x0102;
     if(sizeof (short )==2){
         if(un.c[0]==1&&un.c[1]==2){
-            cout<<"大端"<<endl;
+            cout << "大端" << endl;
         } else if(un.c[1]==1&&un.c[0]==2){
-            cout<<"小端"<<endl;
+            cout << "小端" << endl;
         } else{
-            cout<<"unknown"<<endl;
+            cout << "unknown" << endl;
         }
     } else{
-        cout<<"sizeof(short)="<< sizeof(short)<<endl;
+        cout << "sizeof(short)=" << sizeof(short) << endl;
     }
     return 0;
 
