012d5d6ee893327282638052ebdaa4cd9003475f
sean417 <373202226@qq.com>
2022-01-03 15:19:51 +0800

    endian

--- endian1.cpp
+++ endian1.cpp
@@ -1,4 +1,15 @@
 //
 // Created by chenyang on 2022/1/3.
 //
+#include <iostream>
+
+int main(){
+
+    union {
+        short s;
+        char c[sizeof(short )];
+    } un;
+    un.s=0x0102;
+    
+}
 
